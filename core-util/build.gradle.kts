112d13a0f6b73f8d0a435254484609f518297b52
rino <runo.f.95@gmail.com>
2023-01-22 20:25:01 +0100

    ok

--- core-util/build.gradle.kts
+++ core-util/build.gradle.kts
@@ -49,6 +49,14 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    publishing {
+        singleVariant("release") {
+            // if you don't want sources/javadoc, remove these lines
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
