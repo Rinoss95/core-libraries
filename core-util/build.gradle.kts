8d126e3cd26437f7f3dd8aa44e38d859d272715a
rino <runo.f.95@gmail.com>
2023-01-22 18:07:26 +0100

    Add maven publish

--- core-util/build.gradle.kts
+++ core-util/build.gradle.kts
@@ -1,8 +1,11 @@
 @Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
+    id("maven-publish")
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
+
+group = "com.github.Rinoss95"
 
 android {
     namespace = "com.rinoss95.countries"
@@ -11,8 +14,6 @@ android {
     defaultConfig {
         minSdk = 21
         targetSdk = 33
-        versionCode 1
-        versionName "1.0"
     }
 
     buildTypes {
