d141eca13974d10b54ac64739e69dab910b2e464
rino <runo.f.95@gmail.com>
2023-01-25 16:13:51 +0100

    Remove not needed card

--- sample-app/src/main/java/com/example/SampleAppActivity.kt
+++ sample-app/src/main/java/com/example/SampleAppActivity.kt
@@ -3,15 +3,22 @@ package com.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.theme.MyApplicationTheme
 import com.rinoss95.core_ui.component.list.BaseListItem
 import com.rinoss95.core_ui.component.list.ExpandableListItem
+import com.rinoss95.core_ui.component.text.HeadlineLarge
 import com.rinoss95.core_ui.model.ImageData
 import com.rinoss95.core_ui.util.uiText
 
@@ -21,7 +28,7 @@ class SampleAppActivity : ComponentActivity() {
 
         setContent {
             MyApplicationTheme {
-                BaseListItemPage()
+                SamplePage()
             }
         }
     }
@@ -47,8 +54,10 @@ fun ExampleItem(
 
 @Composable
 @Preview(showBackground = true)
-private fun BaseListItemPage() {
-    Column {
+private fun SamplePage() {
+    Column(
+        Modifier.padding(horizontal = 16.dp),
+    ) {
         ExampleItem()
 
         var isExpanded by remember {
@@ -68,6 +77,33 @@ private fun BaseListItemPage() {
                 ExampleItem()
                 ExampleItem()
             }
+        }
+
+        Box(modifier = Modifier.padding(top = 24.dp))
+
+        ElevatedCard {
+            HeadlineLarge(
+                "ElevatedCard",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        Box(modifier = Modifier.padding(top = 24.dp))
+
+        Card {
+            HeadlineLarge(
+                "FilledCard",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        Box(modifier = Modifier.padding(top = 24.dp))
+
+        OutlinedCard {
+            HeadlineLarge(
+                "OutlinedCard",
+                modifier = Modifier.padding(16.dp)
+            )
         }
     }
 }
