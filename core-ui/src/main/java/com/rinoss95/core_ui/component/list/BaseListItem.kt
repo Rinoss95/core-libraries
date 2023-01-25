d3b317ff41a93b85827d3ef67c2274180716bb2b
rino <runo.f.95@gmail.com>
2023-01-25 16:49:03 +0100

    Add leading to BaseListItem.kt

--- core-ui/src/main/java/com/rinoss95/core_ui/component/list/BaseListItem.kt
+++ core-ui/src/main/java/com/rinoss95/core_ui/component/list/BaseListItem.kt
@@ -1,15 +1,20 @@
 package com.rinoss95.core_ui.component.list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.*
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rinoss95.core_ui.component.ImageComponent
@@ -18,6 +23,7 @@ import com.rinoss95.core_ui.component.text.BodyMedium
 import com.rinoss95.core_ui.component.text.LabelSmall
 import com.rinoss95.core_ui.model.ImageData
 import com.rinoss95.core_ui.R
+import com.rinoss95.core_ui.component.text.TitleMedium
 import com.rinoss95.core_ui.util.uiText
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,6 +34,8 @@ fun BaseListItem(
     supportingText: String = "",
     trailingText: String = "",
     trailingIcon: ImageData? = null,
+    leadingAvatarText: String = "",
+    leadingIcon: ImageData? = null,
     hasDivider: Boolean = false,
     onClick: (() -> Unit)? = null
 ) {
@@ -38,6 +46,7 @@ fun BaseListItem(
     Column(modifier) {
         ListItem(
             modifier = Modifier.then(clickable),
+
             headlineText = {
                 if (headlineText.isNotBlank()) {
                     BodyLarge(
@@ -52,6 +61,30 @@ fun BaseListItem(
                     BodyMedium(
                         supportingText,
                         color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+            },
+
+            leadingContent = {
+                if (leadingAvatarText.isNotBlank()) {
+                    Box(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(CircleShape)
+                            .background(color = MaterialTheme.colorScheme.primaryContainer),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        TitleMedium(
+                            text = leadingAvatarText,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        )
+                    }
+                }
+
+                if (leadingIcon != null) {
+                    ImageComponent(
+                        modifier = Modifier.size(18.dp),
+                        imageData = leadingIcon
                     )
                 }
             },
@@ -95,6 +128,31 @@ private fun BaseListItemPreview() {
             Icons.Filled.KeyboardArrowRight,
             R.string.expand_content.uiText,
         ),
+        hasDivider = true,
+    )
+}
+
+@Preview
+@Composable
+private fun BaseListItemLeadingIconPreview() {
+    BaseListItem(
+        headlineText = "Ciao",
+        supportingText = "Come",
+        leadingIcon = ImageData.IconImageData(
+            Icons.Filled.KeyboardArrowRight,
+            R.string.expand_content.uiText,
+        ),
+        hasDivider = true,
+    )
+}
+
+@Preview
+@Composable
+private fun BaseListItemLeadingAvatarPreview() {
+    BaseListItem(
+        headlineText = "Ciao",
+        supportingText = "Come",
+        leadingAvatarText = "A",
         hasDivider = true,
     )
 }
