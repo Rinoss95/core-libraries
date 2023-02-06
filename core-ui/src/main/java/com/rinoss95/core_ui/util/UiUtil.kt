bf2a0a501d6e098d364d88cfb4b16e05fbca8e2c
rino <runo.f.95@gmail.com>
2023-02-06 20:41:08 +0100

    Add PaddingValues.copy method

--- core-ui/src/main/java/com/rinoss95/core_ui/util/UiUtil.kt
+++ core-ui/src/main/java/com/rinoss95/core_ui/util/UiUtil.kt
@@ -3,10 +3,15 @@ package com.rinoss95.core_ui.util
 import android.content.Context
 import androidx.annotation.PluralsRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import com.rinoss95.core_ui.model.ImageData
 import com.rinoss95.core_ui.model.UiText
 
@@ -66,3 +71,29 @@ inline val @receiver:StringRes Int.uiText: UiText get() = UiText.Resource(this)
 fun @receiver:PluralsRes Int.uiText(count: Int): UiText = UiText.Plural(this, count)
 
 inline val String.uiText: UiText get() = UiText.Plain(this)
+
+fun PaddingValues.start(layoutDirection: LayoutDirection = LayoutDirection.Ltr): Dp {
+    return calculateStartPadding(layoutDirection)
+}
+
+fun PaddingValues.top(): Dp {
+    return calculateTopPadding()
+}
+
+fun PaddingValues.end(layoutDirection: LayoutDirection = LayoutDirection.Ltr): Dp {
+    return calculateEndPadding(layoutDirection)
+}
+
+fun PaddingValues.bottom(): Dp {
+    return calculateBottomPadding()
+}
+
+fun PaddingValues.copy(
+    layoutDirection: LayoutDirection = LayoutDirection.Ltr,
+    start: Dp = start(layoutDirection),
+    top: Dp = top(),
+    end: Dp = end(layoutDirection),
+    bottom: Dp = bottom(),
+): PaddingValues {
+    return PaddingValues(start, top, end, bottom)
+}
