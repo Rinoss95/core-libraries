d3407ab6e7f8138a8dc5a8d1024de6a307aee849
rino <runo.f.95@gmail.com>
2023-02-08 18:21:48 +0100

    Refact ListItemsPage.kt

--- sample-app/src/main/java/com/example/list/ListItemsPage.kt
+++ sample-app/src/main/java/com/example/list/ListItemsPage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.Person
@@ -20,64 +21,6 @@ import com.rinoss95.core_ui.component.list.ExpandableListItem2
 import com.rinoss95.core_ui.component.miscellaneus.TextAvatar
 import com.rinoss95.core_ui.component.text.TitleLarge
 import com.rinoss95.core_ui.sample.R
-
-@Composable
-fun ListItemsPage() {
-    LazyColumn {
-        item {
-            Column(
-                Modifier.padding(bottom = 16.dp),
-            ) {
-                var isExpanded by remember {
-                    mutableStateOf(true)
-                }
-
-                Column {
-                    TitleLarge(
-                        text = "BaseListItem2",
-                        Modifier.padding(top = 24.dp, bottom = 8.dp),
-                    )
-
-                    BaseListItem2(
-                        Modifier
-                            .padding(top = 8.dp)
-                            .clickable(onClick = {}),
-                        headlineText = "Headline text",
-                    )
-
-                    ExpandableListItem2(
-                        headlineText = "Headline Text",
-                        supportingText = "Supporting Text",
-                        hasDivider = isExpanded,
-                        isExpanded = isExpanded,
-                        onClick = {
-                            isExpanded = !isExpanded
-                        }
-                    ) {
-                        Column {
-                            val length = 3
-                            for (i in 1 until length) {
-                                BaseListItem(
-                                    headlineText = "Headline Text",
-                                    supportingText = "Supporting Text",
-                                    hasDivider = i == length - 1,
-                                )
-                            }
-                        }
-                    }
-
-                    for (trailing in trailings) {
-                        BaseListItem2Sets(trailing)
-                    }
-
-                    for (trailing in trailings) {
-                        BaseListItem2Sets(trailing, true)
-                    }
-                }
-            }
-        }
-    }
-}
 
 private val iconPerson = @Composable {
     Icon(
@@ -163,55 +106,120 @@ private val trailings = listOf(
 )
 
 @Composable
-fun BaseListItem2Sets(
+fun ListItemsPage() {
+    var isExpanded by remember {
+        mutableStateOf(true)
+    }
+
+    LazyColumn(Modifier.padding(bottom = 16.dp)) {
+        ExpandableList(isExpanded) {
+            isExpanded = !isExpanded
+        }
+
+        ListItemPreviews()
+    }
+}
+
+fun LazyListScope.ExpandableList(isExpanded: Boolean, onClick: () -> Unit) {
+    item {
+        TitleLarge(
+            text = "Expandable List",
+            Modifier.padding(top = 24.dp),
+        )
+    }
+
+    item {
+        ExpandableListItem2(
+            headlineText = "Headline Text",
+            supportingText = "Supporting Text",
+            hasDivider = isExpanded,
+            isExpanded = isExpanded,
+            onClick = onClick
+        ) {
+            Column {
+                val length = 3
+                for (i in 1 until length) {
+                    BaseListItem(
+                        headlineText = "Headline Text",
+                        supportingText = "Supporting Text",
+                        hasDivider = i == length - 1,
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun LazyListScope.ListItemPreviews() {
+    item {
+        TitleLarge(
+            text = "List Items",
+            Modifier.padding(top = 24.dp),
+        )
+    }
+
+    for (trailing in trailings) {
+        BaseListItem2Sets(trailing)
+    }
+
+    for (trailing in trailings) {
+        BaseListItem2Sets(trailing, true)
+    }
+}
+
+private fun LazyListScope.BaseListItem2Sets(
     trailing: @Composable () -> Unit,
     hasOverline: Boolean = false,
 ) {
-    Column {
-        for (leading in leadings) {
-            if (leading !== trailing) {
-                Column {
-                    BaseListItem2(
-                        Modifier
-                            .padding(top = 8.dp)
-                            .clickable(onClick = {}),
-                        overlineText = if (hasOverline) "Overline text" else "",
-                        headlineText = "Headline text",
-                        leading = leading,
-                    )
+    for (leading in leadings) {
+        if (leading !== trailing) {
+            item {
+                BaseListItem2(
+                    Modifier
+                        .padding(top = 8.dp)
+                        .clickable(onClick = {}),
+                    overlineText = if (hasOverline) "Overline text" else "",
+                    headlineText = "Headline text",
+                    leading = leading,
+                )
+            }
 
-                    BaseListItem2(
-                        Modifier
-                            .padding(top = 8.dp)
-                            .clickable(onClick = {}),
-                        overlineText = "Overline text",
-                        headlineText = "Headline text",
-                        leading = leading,
-                        trailing = trailing,
-                    )
+            item {
+                BaseListItem2(
+                    Modifier
+                        .padding(top = 8.dp)
+                        .clickable(onClick = {}),
+                    overlineText = if (hasOverline) "Overline text" else "",
+                    headlineText = "Headline text",
+                    leading = leading,
+                    trailing = trailing,
+                )
+            }
 
-                    BaseListItem2(
-                        Modifier
-                            .padding(top = 8.dp)
-                            .clickable(onClick = {}),
-                        overlineText = "Overline text",
-                        headlineText = "Headline text",
-                        supportingText = "Supporting text",
-                        leading = leading,
-                        trailing = trailing,
-                    )
+            item {
+                BaseListItem2(
+                    Modifier
+                        .padding(top = 8.dp)
+                        .clickable(onClick = {}),
+                    overlineText = if (hasOverline) "Overline text" else "",
+                    headlineText = "Headline text",
+                    supportingText = "Supporting text",
+                    leading = leading,
+                    trailing = trailing,
+                )
+            }
 
-                    BaseListItem2(
-                        Modifier
-                            .padding(top = 8.dp)
-                            .clickable(onClick = {}),
-                        overlineText = "Overline text",
-                        headlineText = "Headline text",
-                        supportingText = "Very long supporting text that cannot stay on one line",
-                        leading = leading,
-                        trailing = trailing,
-                    )
-                }
+            item {
+                BaseListItem2(
+                    Modifier
+                        .padding(top = 8.dp)
+                        .clickable(onClick = {}),
+                    overlineText = if (hasOverline) "Overline text" else "",
+                    headlineText = "Headline text",
+                    supportingText = "Very long supporting text that cannot stay on one line",
+                    leading = leading,
+                    trailing = trailing,
+                )
             }
         }
     }
@@ -222,53 +230,69 @@ fun BaseListItem2Sets(
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets1() = BaseListItem2Sets(trailings[0])
+private fun BaseListItem2Sets1() = LazyColumn {
+    BaseListItem2Sets(trailings[0])
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets2() = BaseListItem2Sets(trailings[1])
+private fun BaseListItem2Sets2() = LazyColumn {
+    BaseListItem2Sets(trailings[1])
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets3() = BaseListItem2Sets(trailings[2])
+private fun BaseListItem2Sets3() = LazyColumn {
+    BaseListItem2Sets(trailings[2])
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets4() = BaseListItem2Sets(trailings[3])
+private fun BaseListItem2Sets4() = LazyColumn {
+    BaseListItem2Sets(trailings[3])
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets5() = BaseListItem2Sets(trailings[0])
+private fun BaseListItem2Sets5() = LazyColumn {
+    BaseListItem2Sets(trailings[0], true)
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets6() = BaseListItem2Sets(trailings[1])
+private fun BaseListItem2Sets6() = LazyColumn {
+    BaseListItem2Sets(trailings[1], true)
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets7() = BaseListItem2Sets(trailings[2])
+private fun BaseListItem2Sets7() = LazyColumn {
+    BaseListItem2Sets(trailings[2], true)
+}
 
 @Preview(
     showBackground = true,
     heightDp = 2000,
 )
 @Composable
-fun BaseListItem2Sets8() = BaseListItem2Sets(trailings[3])
+private fun BaseListItem2Sets8() = LazyColumn {
+    BaseListItem2Sets(trailings[3], true)
+}
