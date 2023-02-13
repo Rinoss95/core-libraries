2c92bd74f78463bad660f92d3dfc4a19a42e12b7
rino <runo.f.95@gmail.com>
2023-02-13 20:40:36 +0100

    Remove Page.kt, move ErrorComponent.kt, LoadingComponent.kt in separate package

--- core-ui/src/main/java/com/rinoss95/core_ui/component/component/ErrorComponent.kt
+++ core-ui/src/main/java/com/rinoss95/core_ui/component/component/ErrorComponent.kt
@@ -1,4 +1,4 @@
-package com.rinoss95.core_ui.component.page
+package com.rinoss95.core_ui.component.component
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
--- core-ui/src/main/java/com/rinoss95/core_ui/component/component/LoadingComponent.kt
+++ core-ui/src/main/java/com/rinoss95/core_ui/component/component/LoadingComponent.kt
@@ -1,4 +1,4 @@
-package com.rinoss95.core_ui.component.page
+package com.rinoss95.core_ui.component.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
